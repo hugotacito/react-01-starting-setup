ad94ddebfe1fcb0d7f5db0ac17607bb5f5abf9d4
Hugo Tacito <hugotacito@gmail.com>
2023-01-21 10:53:55 -0300

    adding final user management app

--- 01-starting-project-03/src/App.js
+++ 01-starting-project-03/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UserList from "./components/Users/UserList";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const addUserHandler = (username, age) => {
+    setUsers((prev) => {
+      return [
+        ...prev,
+        { name: username, age: age, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={users} />
     </div>
   );
 }
