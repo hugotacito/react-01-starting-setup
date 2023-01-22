35afa6db585471e687e18066e74a5cb660fc4cac
Hugo Tacito <hugotacito@gmail.com>
2023-01-22 18:14:31 -0300

    finishing project of diving in memo and usecallbacks

--- 08-starting-project/src/App.js
+++ 08-starting-project/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
 
-import './App.css';
+import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(true);
+  const [allowToggle, setAllowToggle] = useState(false);
+  console.log("APP RUNNING");
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prev) => !prev);
+    }
+  }, [allowToggle]);
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph</Button>
     </div>
   );
 }
